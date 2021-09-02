890c1dd33be637e9568dd58fd331a9f0cfbbb445
Rayslla <68258208+LaranjaG@users.noreply.github.com>
2021-09-01 21:57:29 -0300

    Update Desafio.cpp

--- 01/Desafio.cpp
+++ 01/Desafio.cpp
@@ -1,40 +1,15 @@
 #include<stdio.h>
 #include <stdlib.h>
 
-int decontarValor(int desconto, int valor){
-	return valor - desconto;
-}
+//Variaveis globais
+int notaCem = 0, 
+	notaCinquenta = 0, 
+	notaVinte = 0, 
+	notaDez = 0, 
+	notaCinco = 0, 
+	notaDois = 0;
 
-void saqueNotasMaioresParaMenores(int valor){
-	int notaCem = 0, 
-		notaCinquenta = 0, 
-		notaVinte = 0, 
-		notaDez = 0, 
-		notaCinco = 0, 
-		notaDois = 0;
-	
-	while(valor != 0){
-		if(valor >= 100){
-			notaCem++;
-			valor = decontarValor(100, valor);
-		} else if(valor >= 50){
-			notaCinquenta++;
-			valor = decontarValor(50, valor);
-		} else if(valor >= 20){
-			notaVinte++;
-			valor = decontarValor(20, valor);
-		} else if(valor >= 10){
-			notaDez++;
-			valor = decontarValor(10, valor);
-		} else if(valor >= 5){	
-			notaCinco++;
-			valor = decontarValor(5, valor);
-		} else {
-			notaDois++;
-			valor = decontarValor(2, valor);
-		}
-	}
-	
+void mostrar(){
 	if(notaCem != 0){
 		printf("\nNotas de 100: %d", notaCem);
 	}
@@ -53,6 +28,33 @@ void saqueNotasMaioresParaMenores(int valor){
 	if(notaDois != 0){
 		printf("\nNotas de 2: %d", notaDois);
 	}
+}
+
+void saqueNotasMaioresParaMenores(int valor){
+	
+	while(valor >= 0){
+		if(valor >= 100){
+			notaCem++;
+			valor -= 100;
+		} else if(valor >= 50){
+			notaCinquenta++;
+			valor -= 50;
+		} else if(valor >= 20){
+			notaVinte++;
+			valor -= 20;
+		} else if(valor >= 10){
+			notaDez++;
+			valor -= 10;
+		} else if(valor >= 5){	
+			notaCinco++;
+			valor -= 5;
+		} else {
+			notaDois++;
+			valor -= 2;
+		}
+	}
+	
+	mostrar();
 }
 
 int qtdNotasUnicas(int nota, int valor){
@@ -85,104 +87,89 @@ void saqueNotasUnicas(int valor){
 void saqueNotasAleatorias(int valor){
 	
 	/**
-	* Est· opÁ„o como È utilizado um valor random para sua execuss„o pode gerar longos loops
+	* Est√° op√ß√£o como √© utilizado um valor random para sua execuss√£o pode gerar longos loops
 	*/
-	
-	int notaCem = 0, 
-		notaCinquenta = 0, 
-		notaVinte = 0, 
-		notaDez = 0, 
-		notaCinco = 0, 
-		notaDois = 0;
 		
 	int notas[6] = {2, 5, 10, 20, 50, 100}; 
 	
-	while(valor != 0){
+	while(valor >= 0){
 		int index = rand() % 6;
 		int notaSaque = notas[index];
 		
-//		printf("\n%d", notaSaque);
-		
-		if(valor % notaSaque == 0){
-			switch(index){
-				case 0: notaDois++; break;
-				case 1: notaCinco++; break;
-				case 2: notaDez++; break;
-				case 3: notaVinte++; break;
-				case 4: notaCinquenta++; break;
-				case 5: notaCem++; break;
-				default: printf("Random n„o deveria ter gerado esse index!"); break;
-			}
+		int proximoValor = valor - notaSaque;
+
+		if(((valor % notaSaque == 0) || (valor >= notaSaque))){
 			
-			valor = decontarValor(notaSaque, valor);
+			if((proximoValor > 3) || ((proximoValor % 2) == 0) || (proximoValor == 0)){
+				switch(index){
+					case 0: notaDois++; break;
+					case 1: notaCinco++; break;
+					case 2: notaDez++; break;
+					case 3: notaVinte++; break;
+					case 4: notaCinquenta++; break;
+					case 5: notaCem++; break;
+					default: printf("Random n√£o deveria ter gerado esse index!"); break;
+				}
+				
+				valor -= notaSaque;
+			} else continue;
 		} else continue;
 	}
 	
-	if(notaCem != 0){
-		printf("\nNotas de 100: %d", notaCem);
-	}
-	if(notaCinquenta != 0){
-		printf("\nNotas de 50: %d", notaCinquenta);
-	}
-	if(notaVinte != 0){
-		printf("\nNotas de 20: %d", notaVinte);
-	}
-	if(notaDez != 0){
-		printf("\nNotas de 10: %d", notaDez);
-	}
-	if(notaCinco != 0){
-		printf("\nNotas de 5: %d", notaCinco);
-	}
-	if(notaDois != 0){
-		printf("\nNotas de 2: %d", notaDois);
-	}
+	mostrar();
 }
 
 int main(void){
 	
 	/** 
-	 * o usu·rio pode sacar valores divisiveis por 5 ou 2, j· que o caixa n„o disponibiliza notas de 1
+	 * O usu√°rio pode sacar valores diferentes de 1 e 3
 	 */
 	int valorSaque = 0;
 	int montanteDinheiro = 5000; // Total de dinheiro disponivel no caixa
 	
 	printf("Montante disponivel no caixa: %d\n", montanteDinheiro);
 	
-	printf("Valor que deseja sacar (um valor inteiro e divisivel por 2 ou 5): ");
+	printf("Valor que deseja sacar (um valor inteiro e divisivel por 2 ou 5 se for menor que 3): ");
 	scanf("%d", &valorSaque);
+	
+	bool executarSaque = (valorSaque % 2 == 0 || valorSaque % 5 == 0);
 	
 	//Formas de saque
 	if(valorSaque > 0 && montanteDinheiro >= valorSaque){	
-		if(valorSaque % 2 == 0 || valorSaque % 5 == 0){
+		if(executarSaque || valorSaque > 3){
 			montanteDinheiro -= valorSaque; //Desconta o valor sacado do dinheiro do caixa
 				
 			int opcao;
 			printf("Opcoes de saque:" 
 				   "\n1 - Saque das maiores notas para menores"
-				   "\n2 - Saque de notas de um mesmo valor para a quantia desejada"
-				   "\n3 - Saque de notas aleatorias"
-				   "\nOpÁ„o: ");
+				   "\n2 - Saque de notas aleatorias"
+				   "%s"
+				   "\nOp√ß√£o: ", 
+				   (executarSaque ? "\n3 - Saque de notas de um mesmo valor para a quantia desejada" : ""));
 					scanf("%d", &opcao);
 			
 			printf("\nSaque:");
 			switch(opcao){
 				case 1:
 					saqueNotasMaioresParaMenores(valorSaque);
-					break;
-				case 2:
-					saqueNotasUnicas(valorSaque);
 					break;	
-				case 3: 
+				case 2: 
 					saqueNotasAleatorias(valorSaque);
 					break;
+				case 3:
+					if (executarSaque)
+						saqueNotasUnicas(valorSaque);
+					else
+						printf("N√£o √© possivel realizar o saque de notas √∫nicas com esse valor!");
+					break;
 				default:
-					printf("OpÁ„o inv·lida!");
+					printf("Op√ß√£o inv√°lida!");
 					break;
 			}
 			
 			printf("\nMontante disponivel no caixa apos saque: %d\n", montanteDinheiro);
 		} else{
-			printf("O valor nao pode ser sacado! Entre com um valor divisivel por 5 ou 2 para realizar o saque!");
+			printf("O valor nao pode ser sacado! Entre com um valor divisivel por 5 ou 2 para realizar o saque ou maior que 3!");
 		}
 	} else{
 		printf((montanteDinheiro < valorSaque) ? "Caixa nao possui dinheiro para sacar esta quantia!" : "Para sacar o valor deve ser maior que 0!");
@@ -190,4 +177,3 @@ int main(void){
 	
 	return 0;
 }
-
